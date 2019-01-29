9197002c52b67404b86a5546080c66a3fe5c1331
José C. García Gamero <jcgarciagamero@gmail.com>
2019-01-29 21:21:45 +0100

    Update NotMINING.desktop_1.0.1.py

--- NotMINING.desktop_1.0.1.py
+++ NotMINING.desktop_1.0.1.py
@@ -1,112 +1,202 @@
-#! /usr/bin/python3
-# -*- coding: utf-8 -*-
-import urllib
-import os
-import sys
-os.system('clear')
+import os, sys, requests
 
 
-print(" _   _       _   __  __ _____ _   _ _____ _   _  _____ ")      
-print("| \ | |     | | |  \/  |_   _| \ | |_   _| \ | |/ ____|")      
-print("|  \| | ___ | |_| \  / | | | |  \| | | | |  \| | |  __ ") 
-print("| . ` |/ _ \| __| |\/| | | | | . ` | | | | . ` | | |_ |")
-print("| |\  | (_) | |_| |  | |_| |_| |\  |_| |_| |\  | |__| |")
-print("|_| \_|\___/ \__|_|  |_|_____|_| \_|_____|_| \_|\_____(")
-print("                                  https://notmining.org")
-print("")
-print("")
-
-print("\x1b[1;37m" + "Select an option: ")
-print("(1): Scan")
-print("(2): Reanalyze")
-print("(3): Blacklist")
-print("")
-print("(0): Exit")
-print("")
-option = input("Option: ")
-
-
-if option == 1:
-	web = input("Enter URL: ")
-	print("Result: ")
-	print("")
-	scan = "https://notmining.org/url?busqueda="+web
-	lines = urllib.urlopen(scan).read()
-
-	search = lines.find("NOT Mining!")
-	search1 = lines.find("Mining!")
-	search2 = lines.find("Enter a valid URL")
-
-	if search != -1:
-		print("Not Mining")
-		print(f"Analysis: https://notmining.org/url?busqueda={web}")
-		print("")
-	elif search1 != -1:
-		print("Mining")
-		print(f"Analysis: https://notmining.org/url?busqueda={web}")
-		print("")
-	elif search2 != -1:
-		print("Enter a valid URL")
-		print("")
-	else:
-		print("The requested website is not available, prevents its analysis, or is incorrect URL.")
-		print(f"Analysis: {web}")
-		print("")
-elif option == 2:
-	web = input("Enter URL: ")
-	scan = "https://notmining.org/analyze?busqueda="+web
-	lines = urllib.urlopen(scan).read()
-
-	search = lines.find("NOT Mining!")
-	search1 = lines.find("Mining!")
-	search2 = lines.find("Enter a valid URL")
-
-	if search != -1:
-		print("Not Mining")
-		print(f"Analysis: https://notmining.org/analyze?busqueda={web}")
-		print("")
-	elif search1 != -1:
-		print("Mining")
-		print(f"Analysis: https://notmining.org/analyze?busqueda={web}")
-		print("")
-	elif search2 != -1:
-		print("Enter a valid URL")
-		print("")
-	else:
-		print("The requested website is not available, prevents its analysis, or is incorrect URL.")
-		print(f"Analysis: https://notmining.org/url?busqueda={web}")
+if os.name == 'nt':
+	while True:
+		os.system('cls')
+		print(" _   _       _   __  __ _____ _   _ _____ _   _  _____ ")      
+		print("| \ | |     | | |  \/  |_   _| \ | |_   _| \ | |/ ____|")      
+		print("|  \| | ___ | |_| \  / | | | |  \| | | | |  \| | |  __ ") 
+		print("| . ` |/ _ \| __| |\/| | | | | . ` | | | | . ` | | |_ |")
+		print("| |\  | (_) | |_| |  | |_| |_| |\  |_| |_| |\  | |__| |")
+		print("|_| \_|\___/ \__|_|  |_|_____|_| \_|_____|_| \_|\_____(")
+		print("                                  https://notmining.org")
+		print("Detect: Windows")
 		print("")
 
-elif option == 3:
-	os.system('clear')
-	web = "https://notmining.org/export.php"
-	lines = urllib.urlopen(web).read()
-	print(lines)
+		print("Select an option: ")
+		print("(1): Scan")
+		print("(2): Reanalyze")
+		print("(3): Blacklist")
+		print("")
+		print("(0): Exit")
+		print("")
+		option = input("Option: ")
 
-elif option == 0:
-	os.system('clear')
-	print(" _   _       _   __  __ _____ _   _ _____ _   _  _____ ")      
-	print("| \ | |     | | |  \/  |_   _| \ | |_   _| \ | |/ ____|")      
-	print("|  \| | ___ | |_| \  / | | | |  \| | | | |  \| | |  __ ") 
-	print("| . ` |/ _ \| __| |\/| | | | | . ` | | | | . ` | | |_ |")
-	print("| |\  | (_) | |_| |  | |_| |_| |\  |_| |_| |\  | |__| |")
-	print("|_| \_|\___/ \__|_|  |_|_____|_| \_|_____|_| \_|\_____(")
-	print("                                  https://notmining.org")
-	print("")
-	print("")
-	print("Credits:")
-	print("")
-	print("NotMINING Desktop")
-	print("Version: 1.0.2")
-	print("Website: http://notmining.org")
-	print("Twitter: @notminingorg")
-	print("E-Mail:  info@notmining.org")
-	print("Created by Jose C. García Gamero)
-	print("Updated by @Manza_Root)
-	sys.exit()
+		if option == "1":
+			web = input("Enter URL: ")
+			print("Result: ")
+			print("")
+			scan = "https://notmining.org/url?busqueda="+web
+			a = requests.get(scan)
+			lines = a.text
 
-else: 
-	print("Select a valid option:")
+			if "NOT Mining!" in lines:
+				print("Not Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Mining!" in lines:
+				print("Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Enter a valid URL" in lines:
+				print("Enter a valid URL")
+				print("")
+			else:
+				print("The requested website is not available, prevents its analysis, or is incorrect URL.")
+				print(f"Analysis: {web}")
+				print("")
+		if option == "2":
+			web = input("Enter URL: ")
+			scan = "https://notmining.org/analyze?busqueda="+web
+			a = requests.get(scan)
+			lines = a.text
 
-	input("Press enter to continue...")
-os.system ("/usr/bin/python NotMINING.desktop_1.0.1.py")
+			if "NOT Mining!" in lines:
+				print("Not Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Mining!" in lines:
+				print("Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Enter a valid URL" in lines:
+				print("Enter a valid URL")
+				print("")
+			else:
+				print("The requested website is not available, prevents its analysis, or is incorrect URL.")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+
+		if option == "3":
+			os.system('cls')
+			web = "https://notmining.org/export.php"
+			a = requests.get(web)
+			lines = a.text
+			print(lines)
+
+		if option == 0:
+			os.system('cls')
+			print(" _   _       _   __  __ _____ _   _ _____ _   _  _____ ")      
+			print("| \ | |     | | |  \/  |_   _| \ | |_   _| \ | |/ ____|")      
+			print("|  \| | ___ | |_| \  / | | | |  \| | | | |  \| | |  __ ") 
+			print("| . ` |/ _ \| __| |\/| | | | | . ` | | | | . ` | | |_ |")
+			print("| |\  | (_) | |_| |  | |_| |_| |\  |_| |_| |\  | |__| |")
+			print("|_| \_|\___/ \__|_|  |_|_____|_| \_|_____|_| \_|\_____(")
+			print("                                  https://notmining.org")
+			print("Detect: UNIX")
+			print("")
+			print("Credits:")
+			print("")
+			print("NotMINING Desktop")
+			print("Version: 1.0.2")
+			print("Website: http://notmining.org")
+			print("Twitter: @notminingorg")
+			print("E-Mail:  info@notmining.org")
+			print("Created by Jose C. García Gamero & Adam K. Martin")
+			sys.exit()
+
+		else: 
+			print("Select a valid option:")
+			input("Press enter to continue...")
+else:
+	while True:
+		os.system('clear')
+		print(" _   _       _   __  __ _____ _   _ _____ _   _  _____ ")      
+		print("| \ | |     | | |  \/  |_   _| \ | |_   _| \ | |/ ____|")      
+		print("|  \| | ___ | |_| \  / | | | |  \| | | | |  \| | |  __ ") 
+		print("| . ` |/ _ \| __| |\/| | | | | . ` | | | | . ` | | |_ |")
+		print("| |\  | (_) | |_| |  | |_| |_| |\  |_| |_| |\  | |__| |")
+		print("|_| \_|\___/ \__|_|  |_|_____|_| \_|_____|_| \_|\_____(")
+		print("                                  https://notmining.org")
+		print("")
+		print("")
+
+		print("\x1b[1;37m" + "Select an option: ")
+		print("(1): Scan")
+		print("(2): Reanalyze")
+		print("(3): Blacklist")
+		print("")
+		print("(0): Exit")
+		print("")
+		option = int(input("Option: "))
+
+
+		if option == "1":
+			web = input("Enter URL: ")
+			print("Result: ")
+			print("")
+			scan = "https://notmining.org/url?busqueda="+web
+						a = requests.get(scan)
+			lines = a.text
+
+			if "NOT Mining!" in lines:
+				print("Not Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Mining!" in lines:
+				print("Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Enter a valid URL" in lines:
+				print("Enter a valid URL")
+				print("")
+			else:
+				print("The requested website is not available, prevents its analysis, or is incorrect URL.")
+				print(f"Analysis: {web}")
+				print("")
+		if option == "2":
+			web = input("Enter URL: ")
+			scan = "https://notmining.org/analyze?busqueda="+web
+						a = requests.get(scan)
+			lines = a.text
+
+			if "NOT Mining!" in lines:
+				print("Not Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Mining!" in lines:
+				print("Mining")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+			elif "Enter a valid URL" in lines:
+				print("Enter a valid URL")
+				print("")
+			else:
+				print("The requested website is not available, prevents its analysis, or is incorrect URL.")
+				print(f"Analysis: https://notmining.org/url?busqueda={web}")
+				print("")
+
+		if option == "3":
+			os.system('clear')
+			web = "https://notmining.org/export.php"
+			a = requests.get(web)
+			lines = a.text
+			print(lines)
+
+		if option == 0:
+			os.system('clear')
+			print(" _   _       _   __  __ _____ _   _ _____ _   _  _____ ")      
+			print("| \ | |     | | |  \/  |_   _| \ | |_   _| \ | |/ ____|")      
+			print("|  \| | ___ | |_| \  / | | | |  \| | | | |  \| | |  __ ") 
+			print("| . ` |/ _ \| __| |\/| | | | | . ` | | | | . ` | | |_ |")
+			print("| |\  | (_) | |_| |  | |_| |_| |\  |_| |_| |\  | |__| |")
+			print("|_| \_|\___/ \__|_|  |_|_____|_| \_|_____|_| \_|\_____(")
+			print("                                  https://notmining.org")
+			print("")
+			print("")
+			print("Credits:")
+			print("")
+			print("NotMINING Desktop")
+			print("Version: 1.0.2")
+			print("Website: http://notmining.org")
+			print("Twitter: @notminingorg")
+			print("E-Mail:  info@notmining.org")
+			print("Created by Jose C. García Gamero")
+			print("Updated by @Manza_Root")
+			sys.exit()
+
+		else: 
+			print("Select a valid option:")
+			input("Press enter to continue...")
+		
